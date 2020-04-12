171c45afd091ad1d7ecf627ea502cc95284855ba
eabuhna <anand.bhupathiraju@ericsson.com>
2020-04-12 02:45:10 +0200

    developemnts to bootstrap k8 server

--- bootstart_new_vm.sh
+++ bootstart_new_vm.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 
-sudo cloud-localds /var/lib/libvirt/images/cloud.img cloud.txt
+#Install the needfull packages.
+#sudo apt install cloud-image-utils virtinst -y
+
+#Make directory to download and cloud-img files
+#mkdir ubuntu18
+
+#Get the cloud image from https://cloud-images.ubuntu.com/
+#cd ubuntu18
+#wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
+
+# Crete the image file ot iso file as required with user input from the text file.
+#cd ../
+sudo cloud-localds /var/lib/libvirt/images/cloud.img cloud.yaml
+
+# Converting?
 sudo qemu-img convert -f qcow2 ubuntu18/bionic-server-cloudimg-amd64.img /var/lib/libvirt/images/U18.04.img
-sudo chmod +x virsh.sh
-./virsh.sh
+
+
--- pre-req.sh
+++ pre-req.sh
@@ -1,19 +1,4 @@
-#Install the needfull packages.
-sudo apt install cloud-image-utils virtinst -y
 
-#Make directory to download and cloud-img files
-mkdir ubuntu18
-
-#Get the cloud image from https://cloud-images.ubuntu.com/
-cd ubuntu18
-wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
-
-# Crete the image file ot iso file as required with user input from the text file.
-cd ../
-sudo cloud-localds /var/lib/libvirt/images/cloud.img cloud.txt
-
-# Converting?
-sudo qemu-img convert -f qcow2 ubuntu18/bionic-server-cloudimg-amd64.img /var/lib/libvirt/images/U18.04.img
 
 #Creating the virtual machine with script using virsh cli commands
 sudo chmod +x virsh.sh
