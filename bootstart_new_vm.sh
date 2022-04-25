cae17800cc83828f9ee859a431af079a8d5f6bde
Anand <anandbhupathiraz@gmail.com>
2022-04-25 21:00:52 +0200

    fix

--- bootstart_new_vm.sh
+++ bootstart_new_vm.sh
@@ -2,6 +2,6 @@
 
 
 sudo cloud-localds /var/lib/libvirt/images/cloud.img cloud.txt
-sudo qemu-img convert -f qcow2 ubuntu18/bionic-server-cloudimg-amd64.img /var/lib/libvirt/images/U18.04.img
+sudo qemu-img convert -f qcow2 ubuntu18/bionic-server-cloudimg-amd64.img /var/lib/libvirt/images/ubuntu18.img
 sudo chmod +x virsh.sh
 ./virsh.sh
